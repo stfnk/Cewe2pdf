b0d4183b3cb125b22916ad3063ff79b31bf7ed36
Stefan <stfnk@users.noreply.github.com>
2020-07-04 11:55:16 +0200

    comments & cleanup

--- Cewe2pdf/pdfWriter.cs
+++ Cewe2pdf/pdfWriter.cs
@@ -4,161 +4,207 @@ using System;
 using System.Linq;
 
 namespace Cewe2pdf {
+
     class pdfWriter {
 
-        System.IO.FileStream _fileStream;
-
-        private Document _doc = new Document();
-
+        private System.IO.FileStream _fileStream;
+        private Document _doc = new Document(); 
         private PdfWriter _writer;
 
         public pdfWriter(string pOutPath) {
+            // TODO add more exception checking...
 
+            // Open file stream for exported pdf
             _fileStream = new System.IO.FileStream(pOutPath, System.IO.FileMode.Create);
 
+            // initialize iTextSharp pdf writer 
             _writer = PdfWriter.GetInstance(_doc, _fileStream);
 
-            _doc.AddAuthor("Foo");
+            // just put something in there, doesn't really matter...
+            _doc.AddAuthor("Cewe2pdf.exe");
             _doc.AddCreator("Cewe2Pdf");
-            _doc.AddTitle("Cewe2PdfTest");
+            _doc.AddTitle("ConvertedCewePhotobook");
 
+            // necessary for loading .ttf it seams
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            // TODO probably should add Cewe/Resources/fonts folder here as well, 
+            // but currently doesn't require Cewe installation location for anything else
+            FontFactory.RegisterDirectory("C:\\Windows\\Fonts"); 
 
-            FontFactory.RegisterDirectory("C:\\Windows\\Fonts");
-
+            // start writing
             _doc.Open();
         }
 
         public void writePage(Page pPage) {
-
+            
+            // page size is given per <fotobook/page>. iTextSharp needs it set before adding page or opening document.
             _doc.SetPageSize(new Rectangle(0f, 0f, pPage.bundleSize.X, pPage.bundleSize.Y));
 
+            // handle first page case
             if (!_doc.IsOpen()) _doc.Open();
             else _doc.NewPage();
 
             // fill background
+            // this currently only supports single color backgrounds, see utils/cewe2data.py & DesignIdData.cs for current implementation
+            // this should be updated to use background images from cewe installation folder directly, but
+            // Cewe backgrounds are .webp files which iTextSharp does not support... silly (and faster) workaround for now. 
             PdfContentByte canvas = _writer.DirectContent;
 
-            // draw left
+            // background color can differ between left and right page, so handle them accordingly.
+            // draw left backrgound
             canvas.Rectangle(0, 0, pPage.bundleSize.X / 2, pPage.bundleSize.Y);
             try {
                 canvas.SetColorFill(DesignIdDatabase.backgroundColors[pPage.backgroundLeft != null ? pPage.backgroundLeft : pPage.backgroundRight]);
             } catch (Exception e) {
+                // in case a background designID is not found in DesignIdData.cs, print information to console
                 canvas.SetColorFill(BaseColor.MAGENTA);
-                Console.WriteLine("Missing background: " + pPage.backgroundLeft);
+                Console.WriteLine("[Error] Missing background DesignID: <" + pPage.backgroundLeft + "> Please report this as an issue.");
             }
             canvas.Fill();
 
-            // draw right
+            // TODO de-duplicate this code...
+            // draw right background
             canvas.Rectangle(0 + pPage.bundleSize.X / 2, 0, pPage.bundleSize.X / 2, pPage.bundleSize.Y);
             try {
                 canvas.SetColorFill(DesignIdDatabase.backgroundColors[(pPage.backgroundRight != null ? pPage.backgroundRight : pPage.backgroundLeft)]);
             } catch (Exception e) {
+                // in case a background designID is not found in DesignIdData.cs, print information to console
                 canvas.SetColorFill(BaseColor.MAGENTA);
-                Console.WriteLine("Missing background: " + pPage.backgroundRight);
+                Console.WriteLine("[Error] Missing background DesignID: <" + pPage.backgroundLeft + "> Please report this as an issue.");
             }
             canvas.Fill();
 
-            // draw content
+            // draw all content areas stored in this page
+            // currently only supports <imagearea> and <textarea> from .mcf
             foreach (Area area in pPage.areas) {
+
                 if (area is ImageArea) {
                     ImageArea imgArea = (ImageArea)area;
+
+                    // if image path was not valid draw magenta outline and print error
                     if (imgArea.path == "NULL") {
 
+                        // calculate rect dimensions
                         float pX = imgArea.rect.X;
                         float pY = pPage.bundleSize.Y - imgArea.rect.Y - imgArea.rect.Height;
+                        Rectangle nullRect = new Rectangle(pX, pY, pX + imgArea.rect.Width, pY + imgArea.rect.Height);
 
-                        Rectangle empty = new Rectangle(pX, pY, pX + imgArea.rect.Width, pY + imgArea.rect.Height);
-                        empty.Border = 1 | 2 | 4 | 8;
-                        empty.BorderColor = BaseColor.LIGHT_GRAY;
-                        empty.BorderWidth = 4.0f;
-                        _writer.DirectContent.Rectangle(empty);
+                        // configure border
+                        nullRect.Border = 1 | 2 | 4 | 8;
+                        nullRect.BorderColor = BaseColor.MAGENTA;
+                        nullRect.BorderWidth = 4.0f;
 
+                        // draw to document
+                        _writer.DirectContent.Rectangle(nullRect);
+
+                        Console.WriteLine("[Error] Image path was NULL.");
                         continue;
                     }
 
+                    // load image file. This takes exif orientation into account
                     System.Drawing.Image sysImg = System.Drawing.Image.FromFile(imgArea.path);
-                    Console.WriteLine("now rotate");
-                    System.Drawing.RotateFlipType rot = ExifRotate(sysImg);
 
-                    // calculate somewhat good scaling
-                    float scale = sysImg.Width / (float)imgArea.rect.Width * 0.5f;
-                    scale = scale < 1.0 ? 1.0f : scale;
-                    Console.WriteLine("calced scale: " + scale);
+                    // calculate somewhat good resizing TODO this should be improved to ensure consistent dpi for all images.
+                    float scale = sysImg.Width / (float)imgArea.rect.Width * 0.5f; // magic number literally defines resolution, the smaller, the higher pixel resolution in final pdf.
+                    scale = scale < 1.0 ? 1.0f : scale; // never scale image up
 
+                    // resize image
                     sysImg = (System.Drawing.Image)(new System.Drawing.Bitmap(sysImg, new System.Drawing.Size((int)(sysImg.Width / scale), (int)(sysImg.Height / scale))));
 
-                    Image img;
-                    try {
-                        img = Image.GetInstance(sysImg, sysImg.RawFormat);
-                    } catch (Exception e) {
-                        sysImg.Save("temp.jpg");
-                        Console.WriteLine("cheat... save to file: temp.jpg");
-                        img = Image.GetInstance("temp.jpg");
-                    }
+                    // this is really silly and slooooow but works for now.
+                    // write System.Drawing.Image to disk and re-read as iTextSharp.Image...
+                    sysImg.Save("temp.jpg");
+                    Image img = Image.GetInstance("temp.jpg");
 
+                    // apply scale as defined in .mcf
                     img.ScalePercent(imgArea.scale * 100.0f * scale);
 
+                    // calculate image position in pdf page
                     float posX = imgArea.rect.X + imgArea.cutout.X;
-                    float posY = pPage.bundleSize.Y - imgArea.rect.Y - imgArea.rect.Height;
+                    float posY = pPage.bundleSize.Y - imgArea.rect.Y - imgArea.rect.Height; // pdf origin is in lower left, mcf origin is in upper left 
 
+                    // yaaaaa... whatever. This way everything fits
                     float cropBottom = img.ScaledHeight - imgArea.rect.Height + imgArea.cutout.Y;
 
-                    //Console.WriteLine("crop: " + imgArea.path + " is x:" + imgArea.cutout.X + " is y:" + imgArea.cutout.Y);
+                    // crop image to mcf specified rect
                     Image cropped = cropImage(img, _writer, -imgArea.cutout.X, cropBottom, imgArea.rect.Width, imgArea.rect.Height);
 
+                    // move to mcf specified position
                     cropped.SetAbsolutePosition(imgArea.rect.X, posY);
 
+                    // draw the image
                     _writer.DirectContent.AddImage(cropped);
 
-                    // draw image border
+                    // draw image border if specified in .mcf
                     if (imgArea.border) {
+                        // TODO mcf as an outside property that is currently not taken into account.
+                        // seems like all borders are 'outside' in photobook.
+                        // iTextSharp draws Borders centered (BorderWidth/2 pixels overlap image)
+                        // this should be corrected.
+
+                        // calc border rect
                         float pX = imgArea.rect.X;
                         float pY = pPage.bundleSize.Y - imgArea.rect.Y - imgArea.rect.Height;
-
                         Rectangle rect = new Rectangle(pX, pY, pX + imgArea.rect.Width, pY + imgArea.rect.Height);
 
+                        // convert .mcf's html style color hex code to Color, based on: https://stackoverflow.com/a/2109904
                         int argb = Int32.Parse(imgArea.borderColor.Replace("#", ""), System.Globalization.NumberStyles.HexNumber);
                         System.Drawing.Color clr = System.Drawing.Color.FromArgb(argb);
 
+                        // configure border
                         rect.Border = 1 | 2 | 4 | 8;
                         rect.BorderColor = new BaseColor(clr);
                         rect.BorderWidth = imgArea.borderWidth;
 
+                        // draw border
                         _writer.DirectContent.Rectangle(rect);
                     }
 
-                } else if (area is TextArea) {
+                } 
+                else if (area is TextArea) {
                     TextArea textArea = (TextArea)area;
-                    if (textArea.text == "") continue;
 
+                    // just in case something went wrong
+                    if (textArea.text == "") {
+                        Console.WriteLine("[Error] Text was empty.");
+                        continue;
+                    }
+
+                    // iTextSharp textbox
                     ColumnText colText = new ColumnText(_writer.DirectContent);
-
+                    
+                    // calculate rect
                     float llx = textArea.rect.X;
                     float lly = pPage.bundleSize.Y - textArea.rect.Y - textArea.rect.Height;
-
                     float urx = llx + textArea.rect.Width;
                     float ury = lly + textArea.rect.Height;
-
+                    Rectangle textRect = new Rectangle(llx, lly, urx, ury);
+                    
+                    // convert .mcf's html style color hex code to Color, based on: https://stackoverflow.com/a/2109904
                     int argb = Int32.Parse(textArea.color.Replace("#", ""), System.Globalization.NumberStyles.HexNumber);
                     System.Drawing.Color clr = System.Drawing.Color.FromArgb(argb);
 
+                    // load the correct font
+                    // NOTE: this only works if font is registered from Fonts directory. See constructor.
                     Font font = FontFactory.GetFont(textArea.font, textArea.fontsize, new BaseColor(clr));
 
-                    Rectangle textRect = new Rectangle(llx, lly, urx, ury);
+                    // For testing draw text box outline
+                    // textRect.Border = 1|2|4|8;
+                    // textRect.BorderColor = BaseColor.RED;
+                    // textRect.BorderWidth = 1.0f;
+                    // _writer.DirectContent.Rectangle(textRect);
 
-#if DEBUG_DRAW
-                    textRect.Border = 1|2|4|8;
-                    textRect.BorderColor = BaseColor.RED;
-                    textRect.BorderWidth = 1.0f;
-                    _writer.DirectContent.Rectangle(textRect);
-#endif
+                    // apply rect to textbox
                     colText.SetSimpleColumn(textRect);
 
+                    // the actual text object
                     Paragraph par = new Paragraph(textArea.text, font);
 
-
+                    // magic number that closely matches photobook
+                    // TODO there is probably more information in the .mcf's css part
                     par.SetLeading(0, 1.3f);
+
+                    // apply corrent alignment
                     if (textArea.align == "ALIGNHCENTER")
                         par.Alignment = Element.ALIGN_CENTER;
                     else if (textArea.align == "ALIGNLEFT")
@@ -166,13 +212,17 @@ namespace Cewe2pdf {
                     else if (textArea.align == "ALIGNRIGHT")
                         par.Alignment = Element.ALIGN_RIGHT;
 
+                    // add paragraph to textbox
                     colText.AddElement(par);
+
+                    // draw textbox
                     colText.Go();
                 }
             }
         }
 
         public Image cropImage(Image image, PdfWriter writer, float fromLeft, float fromBottom, float width, float height) {
+            // from https://stackoverflow.com/a/14473667
             PdfContentByte cb = writer.DirectContent;
             PdfTemplate t = cb.CreateTemplate(width, height);
             float origWidth = image.ScaledWidth;
@@ -182,17 +232,19 @@ namespace Cewe2pdf {
         }
 
         public void close() {
+            // close all files and filestreams...
             _doc.Close();
             _writer.Close();
             _fileStream.Close();
-            Console.WriteLine("Wrote pdf.");
         }
 
-
-        private const int exifOrientationID = 0x112; //274
-        private const int ExifOrientationTagId = 0x112; //274
-
         public System.Drawing.RotateFlipType ExifRotate(System.Drawing.Image img) {
+            // for some reason iText does not respect orientation stored in metadata as it seams... 
+            // try to fix it with this weird stuff...
+            // based on https://www.cyotek.com/blog/handling-the-orientation-exif-tag-in-images-using-csharp
+
+            const int exifOrientationID = 0x112; //274
+
             if (!img.PropertyIdList.Contains(exifOrientationID)) {
                 Console.WriteLine("no exif info found.");
                 return System.Drawing.RotateFlipType.RotateNoneFlipNone;
