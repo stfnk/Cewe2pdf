c2ead1b1c3fa4571c52d244226228a8358209b1d
Stefan <stfnk@users.noreply.github.com>
2020-07-22 17:41:26 +0200

    colored logs, better defaults

--- Cewe2pdf/Log.cs
+++ Cewe2pdf/Log.cs
@@ -11,32 +11,40 @@ namespace Cewe2pdf {
             Info,
         };
 
-        public static Level level = Level.Message;
+        public static Level level = Level.Info;
 
         private static string log = "\nPlease attach this file to bug reports.\n\n";
 
         public static void Message(string message, bool keepLine = false) {
-            log += "[Message] " + message + (keepLine ? "" : "\n");
-            if ((int)level >= (int)Level.Message)
+            log += "[Message]\t" + message + (keepLine ? "" : "\n");
+            if ((int)level >= (int)Level.Message) {
+                Console.ForegroundColor = ConsoleColor.White;
                 if (keepLine)
                     Console.Write(message);
                 else
                     Console.WriteLine(message);
+            }
         }
 
         public static void Error(string message) {
-            log += "[Error]   " + message + "\n";
-            if ((int)level >= (int)Level.Error) Console.WriteLine("[Error] " + message);
+            log += "[Error]\t" + message + "\n";
+            Console.ForegroundColor = ConsoleColor.Red;
+            if ((int)level >= (int)Level.Error) Console.WriteLine("[Error]   " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void Warning(string message) {
-            log += "[Warning] " + message + "\n";
+            log += "[Warning]\t" + message + "\n";
+            Console.ForegroundColor = ConsoleColor.Yellow;
             if ((int)level >= (int)Level.Warning) Console.WriteLine("[Warning] " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void Info(string message) {
-            log += "[Info]    " + message + "\n";
-            if ((int)level >= (int)Level.Info) Console.WriteLine("[Info] " + message);
+            log += "[Info]\t" + message + "\n";
+            Console.ForegroundColor = ConsoleColor.Gray;
+            if ((int)level >= (int)Level.Info) Console.WriteLine("[Info]    " + message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void writeLogFile() {
