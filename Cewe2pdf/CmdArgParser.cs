f5b329ecdf72f54e43676a3c04b35674f0c78d64
Stefan <stfnk@users.noreply.github.com>
2021-01-11 13:21:05 +0100

    move argument parsing to own class

---
+++ Cewe2pdf/CmdArgParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cewe2pdf
+{
+    class CmdArgParser {
+        public static void parse(string[] args)
+        {
+            // handle arguments, overwriting config
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-h" || args[i] == "--help")
+                {
+                    Console.WriteLine(
+                      "Cewe2pdf usage:"
+                      + "\n\t    argument | description\n"
+                      + "\n\t      --help | lists all arguments."
+                      + "\n\t          -h | same as --help."
+                      + "\n\t    'in.mcf' | input file to convert (required)"
+                      + "\n\t   'out.pdf' | output file to generate (optional)"
+                      + "\n\t  -to-page 0 | convert only up to this page nr, 0 converts all."
+                      + "\n\t-quality 1.0 | pixel size of images. Use lower value for higher resolution images in .pdf." // TODO: true?
+                    );
+                }
+                else if (args[i].EndsWith(".mcf")) Program.mcfPath = args[i];
+                else if (args[i].EndsWith(".pdf")) Program.pdfPath = args[i];
+                else if (args[i] == "-to-page")
+                {
+                    i++;
+                    if (i < args.Length) Config.toPage = Math.Max(0, Convert.ToInt32(args[i]));
+                }
+                else if (args[i] == "-quality")
+                {
+                    i++;
+                    if (i < args.Length) Config.imgScale = Math.Clamp(float.Parse(args[i], System.Globalization.CultureInfo.InvariantCulture), 0.0f, 1.0f); // TODO: true?
+                }
+                else
+                {
+                    Log.Warning("invalid argument: '" + args[i] + "'.");
+                }
+            }
+        }
+    }
+}
--- Cewe2pdf/Program.cs
+++ Cewe2pdf/Program.cs
@@ -4,46 +4,20 @@ namespace Cewe2pdf {
 
     class Program {
 
-        static string mcfPath = "";
-        static string pdfPath = "";
+        public static string mcfPath = "";
+        public static string pdfPath = "";
 
         static void Main(string[] args) {
 
             // initializes config with either defaults or from config file
             Config.initialize();
 
-            // handle arguments, overwriting config
-            for (int i = 0; i < args.Length; i++) {
-                if (args[i] == "-h" || args[i] == "--help") {
-                  Console.WriteLine(
-                    "Cewe2pdf usage:"
-                    "\n\t    argument | description\n"
-                    "\n\t      --help | lists all arguments."
-                    "\n\t          -h | same as --help."
-                    "\n\t    'in.mcf' | input file to convert (required)"
-                    "\n\t   'out.pdf' | output file to generate (optional)"
-                    "\n\t  -to-page 0 | convert only up to this page nr, 0 converts all."
-                    "\n\t-quality 1.0 | pixel size of images. Use lower value for higher resolution images in .pdf." // TODO: true?
-                  );
-                }
-                else if (args[i].EndsWith(".mcf")) mcfPath = args[i];
-                else if (args[i].EndsWith(".pdf")) pdfPath = args[i];
-                else if (args[i] == "-to-page") {
-                  i++;
-                  if (i < args.Length) config.toPage = MathF.Max(0, ToInt32(args[i]));
-                }
-                else if (args[i] == "-quality") {
-                  i++;
-                  if (i < args.Length) config.toPage = MathF.Clamp(ToFloat(args[i]), 0.0f, 1.0f); // TODO: true?
-                }
-                else {
-                  Log.Warning("invalid argument: '" + args[i] + "'.");
-                }
-            }
+            // overwrites config with arguments if given
+            CmdArgParser.parse(args);
 
             // check for valid input file
             if (mcfPath == "") { Log.Error("no input.mcf file specified."); return; }
-            if (!File.Exists(mcfPath)) { Log.Error("'" + mcfPath + "' does not exist.'"); return; }
+            if (!System.IO.File.Exists(mcfPath)) { Log.Error("'" + mcfPath + "' does not exist.'"); return; }
 
             // allow only input file as argument
             if (pdfPath == "") pdfPath = mcfPath.Replace(".mcf", ".pdf");
@@ -55,8 +29,8 @@ namespace Cewe2pdf {
             DesignIdConverter.initDesignIdDatabase();
 
             // initialize with given files
-            mcfParser parser = new mcfParser(Config.mcfPath);
-            pdfWriter writer = new pdfWriter(Config.pdfPath);
+            mcfParser parser = new mcfParser(mcfPath);
+            pdfWriter writer = new pdfWriter(pdfPath);
 
             if (Config.toPage > 0)
                 Log.Message("Converting " + Config.toPage.ToString() + " pages.");
@@ -81,9 +55,9 @@ namespace Cewe2pdf {
             }
 
             // close files
-            Log.Message("Saving '" + Config.pdfPath + "'.");
+            Log.Message("Writing '" + pdfPath + "'.");
             writer.close();
-            Log.Message("Finished in " + timer.ElapsedMilliseconds/1000.0f + " seconds.");
+            Log.Message("Conversion finished in " + timer.ElapsedMilliseconds/1000.0f + " seconds.");
             Log.writeLogFile();
         }
     }
